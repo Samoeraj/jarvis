6b0405f0d181617c267ed3d1a8ed6d85c8f92610
Sam Timmermans <sammy.timmermans@gmail.com>
2025-11-16 18:12:53 +0100

    added sysinfo dependency

---
+++ backend/target/debug/deps/either-722c25d9f92ec84b.d
@@ -0,0 +1,9 @@
+/Users/samtimmermans/Desktop/Coding/jarvis/backend/target/debug/deps/either-722c25d9f92ec84b.d: /Users/samtimmermans/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/either-1.15.0/src/lib.rs /Users/samtimmermans/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/either-1.15.0/src/iterator.rs /Users/samtimmermans/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/either-1.15.0/src/into_either.rs
+
+/Users/samtimmermans/Desktop/Coding/jarvis/backend/target/debug/deps/libeither-722c25d9f92ec84b.rlib: /Users/samtimmermans/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/either-1.15.0/src/lib.rs /Users/samtimmermans/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/either-1.15.0/src/iterator.rs /Users/samtimmermans/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/either-1.15.0/src/into_either.rs
+
+/Users/samtimmermans/Desktop/Coding/jarvis/backend/target/debug/deps/libeither-722c25d9f92ec84b.rmeta: /Users/samtimmermans/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/either-1.15.0/src/lib.rs /Users/samtimmermans/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/either-1.15.0/src/iterator.rs /Users/samtimmermans/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/either-1.15.0/src/into_either.rs
+
+/Users/samtimmermans/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/either-1.15.0/src/lib.rs:
+/Users/samtimmermans/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/either-1.15.0/src/iterator.rs:
+/Users/samtimmermans/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/either-1.15.0/src/into_either.rs:
---
+++ backend/target/debug/deps/jarvis_backend-dc204852c68ac799.d
@@ -0,0 +1,5 @@
+/Users/samtimmermans/Desktop/Coding/jarvis/backend/target/debug/deps/jarvis_backend-dc204852c68ac799.d: src/main.rs
+
+/Users/samtimmermans/Desktop/Coding/jarvis/backend/target/debug/deps/jarvis_backend-dc204852c68ac799: src/main.rs
+
+src/main.rs:
